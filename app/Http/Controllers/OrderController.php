3e80319680f675f83d403c39c76c78fd4cef4653
makljenovicana <makljenovicana@gmail.com>
2024-04-13 20:47:53 +0200

    Dodati kontroleri

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,22 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\order;
+//use App\Http\Resources\OrderResource;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class OrderController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
         $orders = Order::all();
-    return $orders;
+        return $orders;
     }
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -26,14 +32,37 @@ class OrderController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'location' => 'required|string|max:255',
+            'customer_id' => 'required|integer'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $order = Order::create([
+            'location' => $request->location,
+            'customer_id' => $request->customer_id
+        ]);
+
+        return response()->json([
+            'message' => 'Order was created',
+            'order' => new OrderResource($order)
+        ]);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
      */
     public function show($order_id)
     {
@@ -46,25 +75,52 @@ class OrderController extends Controller
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
      */
-    public function edit(order $order)
+    public function edit(Order $order)
     {
         //
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, order $order)
+    public function update(Request $request, Order $order)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'location' => 'required|string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $order->location = $request->location;
+
+        $order->save();
+
+        return response()->json([
+            'message' => 'Order was updated',
+            'order' => new OrderResource($order)
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(order $order)
+    public function destroy(Order $order)
     {
-        //
+        $order->delete();
+
+        return response()->json('Order was deleted');
     }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\product;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
@@ -18,6 +22,8 @@ class ProductController extends Controller
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -26,45 +32,99 @@ class ProductController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'price' => 'required|integer|between:10,300',
+            'order_id' => 'required|integer'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $product = Product::create([
+            'name' => $request->name,
+            'price' => $request->price,
+            'order_id' => $request->order_id,
+        ]);
+
+        return response()->json([
+            'message' => 'Product was created',
+            'product' => new ProductResource($product)
+        ]);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
      */
     public function show($product_id)
     {
         $product = Product::find($product_id);
         if (is_null($product)) {
-            return response()->json('product not found', 404);
+            return response()->json('Product not found', 404);
         }
         return response()->json($product);
     }
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
      */
-    public function edit(product $product)
+    public function edit(Product $product)
     {
         //
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, product $product)
+    public function update(Request $request, Product $product)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'price' => 'required|integer|between:10,300',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $product->name = $request->name;
+        $product->price = $request->price;
+
+        $product->save();
+
+        return response()->json([
+            'message' => 'Product was updated',
+            'product' => new ProductResource($product)
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(product $product)
+    public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return response()->json('Product was deleted');
     }
 }
